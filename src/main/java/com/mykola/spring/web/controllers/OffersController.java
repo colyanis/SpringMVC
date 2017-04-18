3c1558add28058b9e8e43b228963068feac3f38a
colyanis <colyanis@gmail.com>
2017-04-18 17:21:05 +0300

    Updated.

--- src/main/java/com/mykola/spring/web/controllers/OffersController.java
+++ src/main/java/com/mykola/spring/web/controllers/OffersController.java
@@ -1,6 +1,7 @@
 package com.mykola.spring.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class OffersController {
 
     @RequestMapping("/")
-    public String showHome() {
+    public String showHome(Model model) {
+        model.addAttribute("name", "River");
+
         return "home";
     }
 }
