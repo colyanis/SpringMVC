a67f43acc4ad3cd1aa3bca98ecd84ea19d41afef
colyanis <colyanis@gmail.com>
2017-05-09 22:11:38 +0300

    126. Added test crete user.

--- src/test/java/com/mykola/spring/web/test/UserDaoTests.java
+++ src/test/java/com/mykola/spring/web/test/UserDaoTests.java
@@ -1,13 +1,21 @@
 package com.mykola.spring.web.test;
 
 
+import com.mykola.spring.web.dao.User;
+import com.mykola.spring.web.dao.UsersDao;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.sql.DataSource;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @ActiveProfiles("dev")
 @ContextConfiguration(locations = {"classpath:datasource.xml",
@@ -15,8 +23,26 @@ import static org.junit.Assert.assertEquals;
         "classpath:config/security-context.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UserDaoTests {
+    @Autowired
+    private UsersDao usersDao;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Before
+    public void init() {
+        JdbcTemplate jdbc = new JdbcTemplate(dataSource);
+
+        jdbc.execute("truncate users");
+        jdbc.execute("truncate authorities");
+    }
+
     @Test
     public void testCreateUser() {
-         assertEquals("Dummy test", 1, 1);
+        User user = new User("jedai1", "hello", "jedai@j.com", true, "user");
+
+        boolean result = usersDao.create(user);
+
+        assertTrue(result);
     }
 }
