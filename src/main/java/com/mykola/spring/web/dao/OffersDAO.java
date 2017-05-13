ebd7bd82732335390f3e517bdebafda6e465ee0f
colyanis <colyanis@gmail.com>
2017-05-13 22:16:13 +0300

    136. Before Hibernate.

--- src/main/java/com/mykola/spring/web/dao/OffersDAO.java
+++ src/main/java/com/mykola/spring/web/dao/OffersDAO.java
@@ -1,10 +1,9 @@
 package com.mykola.spring.web.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.*;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -21,37 +20,65 @@ public class OffersDao {
         this.jdbc = new NamedParameterJdbcTemplate(jdbc);
     }
 
-    public List<Offer> getOffers() {
-        return jdbc.query("select * from offers", (resultSet, rowNum) -> {
-            Offer offer = new Offer();
-            offer.setId(resultSet.getInt("id"));
-            offer.setName(resultSet.getString("name"));
-            offer.setText(resultSet.getString("text"));
-            offer.setEmail(resultSet.getString("email"));
+    public List<Offer> getOffers(String username) {
+        return jdbc.query("select * from offers, users where offers.username = users.username",
+                new MapSqlParameterSource("username", username), new OfferRowMapper());
+    }
 
-            return offer;
-        });
+    public List<Offer> getOffers() {
+        return jdbc.query("select * from offers, users where offers.username = users.username", new OfferRowMapper());
     }
 
     public boolean create(Offer offer) {
         BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(offer);
 
-        return jdbc.update("insert into offers (name, email, text) values (:name, :email, :text)", params) == 1;
+        return jdbc.update("insert into offers (username, text) values (:username, :text)", params) == 1;
     }
 
-    public Offer getOffer(int i) {
+    @Transactional
+    public int[] create(List<Offer> offers) {
+        SqlParameterSource[] params = SqlParameterSourceUtils.createBatch(offers.toArray());
+        return jdbc.batchUpdate("insert into offers (username, text) values (:username, :text)", params);
+    }
+
+
+    public boolean update(Offer offer) {
+        BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(offer);
+
+        return jdbc.update("update offers set text = :text where id = :id", params) == 1;
+    }
+
+
+
+    public Offer getOffer(int id) {
         MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id", id);
+
+        return jdbc.query("select * from offers, users where " +
+                        "offers.username = users.username and id=:id", params,
+                (resultSet, rowNumber) -> {
+                    User user = new User();
+                    user.setEmail(resultSet.getString("email"));
+                    user.setName(resultSet.getString("name"));
+                    user.setAuthority(resultSet.getString("authority"));
+                    user.setEnabled(resultSet.getBoolean("enabled"));
+                    user.setUsername(resultSet.getString("username"));
 
 
-        return jdbc.query("select * from offers where id=:id", params,
-                (rs, rowNumber) -> {
                     Offer offer = new Offer();
-                    offer.setId(rs.getInt("id"));
-                    offer.setName(rs.getString("name"));
-                    offer.setEmail(rs.getString("id"));
-                    offer.setText(rs.getString("text"));
+                    offer.setId(resultSet.getInt("id"));
+                    offer.setText(resultSet.getString("text"));
+                    offer.setUser(user);
+
                     return offer;
                 }).stream().findAny().orElse(null);
 
     }
+
+    public boolean delete(int id) {
+        MapSqlParameterSource params = new MapSqlParameterSource("id", id);
+
+        return jdbc.update("delete from offers where id=:id", params) == 1;
+    }
+
 }
